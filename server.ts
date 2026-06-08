05b3e31a19ea185c2c5c4ba258125b2ce52d6bfd
Prakash <prakashrajbanc1@gmail.com>
2026-06-08 21:50:00 +0545

    express server connection established

---
+++ server.ts
@@ -0,0 +1,11 @@
+import app from "./src/app";
+import { envConfig } from "./src/config/config";
+
+function startServer() {
+    const port = envConfig.port || 3000
+    app.listen(port, () => {
+        console.log(`Server is listening to the port ${port}`);
+    })
+}
+
+startServer()
