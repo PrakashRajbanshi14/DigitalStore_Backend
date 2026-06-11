8de36a035f4da6d56571fc2fcd9532cc338c5c6d
Prakash <prakashrajbanc1@gmail.com>
2026-06-11 22:19:08 +0545

    Reset Password Feature Added

---
+++ src/services/checkOtpExpiration.ts
@@ -0,0 +1,16 @@
+import { Response } from "express"
+import sendResponse from "./sendResponse"
+
+const checkOtpExpiration = (res:Response,otpGeneratedTime:string,thresholdTime:number)=>{
+    const currentTime = Date.now()
+    if(currentTime - parseInt(otpGeneratedTime)  <= thresholdTime){
+        // otp expires vako xainw
+        sendResponse(res,200,"Valid OTP, now you can proceed to reset password 😌")
+    }else{
+        //otp expires vayo
+        sendResponse(res,403,"OTP expiredd, Sorry try again later 😭!!")
+
+    }
+}
+
+export default checkOtpExpiration
