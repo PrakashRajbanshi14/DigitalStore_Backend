60278f34b4a873b4aaa9af763a4f0e551784ad0b
Prakash <prakashrajbanc1@gmail.com>
2026-06-14 16:05:30 +0545

    authorization middleware restrictTo added

--- src/middleware/userMiddleware.ts
+++ src/middleware/userMiddleware.ts
@@ -1,8 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from 'jsonwebtoken'
 import { envConfig } from "../config/config";
+import User from "../database/models/userModel";
+
+export enum Role{
+    Admin ='admin',
+    Customer = 'customer'
+}
+
+interface IExtendedRequest extends Request{
+    user? : {
+        id: string,
+        username : string,
+        email: string,
+        password: string,
+        role : string
+    }
+}
+
 class UserMiddleware {
-    async isUserLoggedIn(req: Request,res:Response, next:NextFunction):Promise<void>{
+    async isUserLoggedIn(req: IExtendedRequest, res:Response, next:NextFunction):Promise<void>{
         // receive token
         const token = req.headers.authorization
         if(!token){
@@ -13,17 +30,32 @@ class UserMiddleware {
         }
 
         //validate token
-        jwt.verify(token, envConfig.jwtSecretKey as string, async(err, result)=>{
+        jwt.verify(token, envConfig.jwtSecretKey as string, async(err, result:any)=>{
             if(err){
                 res.status(403).json({
                     message: "Invalid token"
                 })
             }else{
-               // console.log(result)
+                const userData = await User.findByPk(result.userId)
+                if(!userData){
+                    res.status(404).json({
+                        message : "No user with that useerId"
+                    })
+                    return
+                }
+                req.user = userData
                 next()
             }
         })
     }
+
+    async restrictTo(...roles:Role[]){
+        return  (req: IExtendedRequest, res: Response, next:NextFunction)=>{
+            let userRole = req.user?.role as Role
+            console.log("Role" + userRole);
+        }
+    }
+
 }
 
 export default new UserMiddleware
--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -1,9 +1,14 @@
 import express, { Router } from 'express'
 import categoryController from '../controllers/categoryController'
-import userMiddleware from '../middleware/userMiddleware'
+import userMiddleware, { Role } from '../middleware/userMiddleware'
 const router:Router = express.Router()
 
-router.route("/").get(categoryController.getCategories).post(userMiddleware.isUserLoggedIn, categoryController.addCategory)
-router.route("/:id").patch(userMiddleware.isUserLoggedIn, categoryController.updateCategory).delete(userMiddleware.isUserLoggedIn, categoryController.deleteCategory)
+router.route("/")
+	.get(categoryController.getCategories)
+	.post(userMiddleware.isUserLoggedIn as any, userMiddleware.restrictTo(Role.Admin) as any, categoryController.addCategory)
+    
+router.route("/:id")
+    .patch(userMiddleware.isUserLoggedIn,userMiddleware.restrictTo(Role.Admin) as any, categoryController.updateCategory)
+    .delete(userMiddleware.isUserLoggedIn,userMiddleware.restrictTo(Role.Admin) as any, categoryController.deleteCategory)
 
-export default router
+export default router 
