011e7674deeab2f487b1ce8766dae73b2f73e13a
Prakash <prakashrajbanc1@gmail.com>
2026-06-17 10:25:33 +0545

    errorHandler implemented on routes

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -2,16 +2,17 @@ import express, { Router } from 'express'
 import productController from '../controllers/productController'
 import userMiddleware, { Role } from '../middleware/userMiddleware'
 import {multer, storage} from "../middleware/multerMiddleware"
+import errorHandler from '../services/errorHandler'
 const upload = multer({storage: storage})
 const router:Router = express.Router()
 
 router.route("/")
-    .get( productController.getAllProducts)
-    .post(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin),upload.single('productImage'), productController.createProduct)
+    .get( errorHandler(productController.getAllProducts))
+    .post(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin),upload.single('productImage'), errorHandler(productController.createProduct))
 
 router.route("/:id")
-    .get(productController.getSingleProduct)
-    .put(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), upload.single('productImage'), productController.updateProduct)
-    .delete(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), productController.deleteProduct)
+    .get(errorHandler(productController.getSingleProduct))
+    .put(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), upload.single('productImage'), errorHandler(productController.updateProduct))
+    .delete(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), errorHandler(productController.deleteProduct))
 
 export default router 
