fadfccd3608c87c630bbd8c7571f97a59602d9b5
Prakash <prakashrajbanc1@gmail.com>
2026-06-17 11:15:46 +0545

    PaymentModel created and global enums defined

---
+++ src/database/models/paymentModel.ts
@@ -0,0 +1,32 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript'
+import { PaymentMethod, PaymentStatus } from '../../globals/types'
+
+
+@Table({
+    tableName: "payments",
+    modelName: "Payment",
+    timestamps: true
+})
+
+class Payment extends Model {
+    @Column({
+        primaryKey : true,
+        type: DataType.UUID,
+        defaultValue : DataType.UUIDV4
+    })
+    declare id : string
+
+    @Column({
+        type: DataType.ENUM(PaymentMethod.Khalti,PaymentMethod.Esewa,PaymentMethod.COD),
+        defaultValue: PaymentMethod.COD
+    })
+    declare paymentMethod : string
+
+    @Column({
+        type: DataType.ENUM(PaymentStatus.Paid,PaymentStatus.Unpaid),
+        defaultValue : PaymentStatus.Unpaid
+    })
+    declare paymentstatus : string
+}
+
+export default Payment
---
+++ src/globals/types/index.ts
@@ -0,0 +1,19 @@
+
+export enum OrderStatus{
+    Preparation = "preparation",
+    Ontheway = "ontheway",
+    Delivered = "Delivered",
+    Pending = "pending",
+    Cancelled = "cancelled"
+}
+
+export enum PaymentMethod{
+    Khalti ="khalti",
+    Esewa = "esewa",
+    COD = "cod"
+}
+
+export enum PaymentStatus{
+    Paid = "Paid",
+    Unpaid ="unpaid"
+}
