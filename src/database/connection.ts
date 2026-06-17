77452821b686a2316a20b8635f7fab52b02c469d
Prakash <prakashrajbanc1@gmail.com>
2026-06-17 12:18:56 +0545

    Relationships built between tables

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,6 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import { envConfig } from "../config/config";
 import Product from "./models/productModel";
 import Category from "./models/categoryModel";
+import Order from "./models/orderModel";
+import User from "./models/userModel";
+import Payment from "./models/paymentModel";
+import OrderDetails from "./models/orderDetails";
 
 const sequelize = new Sequelize(envConfig.connectionString as string, {
     models: [__dirname + '/models']
@@ -24,7 +28,24 @@ sequelize.sync({ force: false, alter: false })  // alter : true helps to keep th
 })
 
 // relationships
+// CategoryId on ProductTable
 Product.belongsTo(Category,{foreignKey: "categoryId"})  // in product table categoryid as a foreign key will be stored
 Category.hasOne(Product,{foreignKey: "categoryId"})
+
+// UserId on Ordertable
+User.hasMany(Order, {foreignKey: "userId"})
+Order.belongsTo(User,{foreignKey: "userId"})
+
+//  orderId on PaymentTable
+Payment.belongsTo(Order, {foreignKey: "orderId"})
+Order.hasOne(Payment, {foreignKey: "orderId"})
+
+// productId on OrderDetailsTable
+OrderDetails.belongsTo(Product,{foreignKey: "productid"})
+Product.hasMany(OrderDetails, {foreignKey: "productId"})
+
+// orderId on OrderDetails Table
+OrderDetails.belongsTo(Order, {foreignKey: "orderId"})
+Order.hasOne(OrderDetails, {foreignKey: "orderId"})
 
 export default sequelize;
